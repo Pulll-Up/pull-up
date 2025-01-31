9fe14ab9f2c61204b437cb99b441ab6728b4bb43
im2shandyou@gmail.com <104557685+im2sh@users.noreply.github.com>
2025-01-31 16:53:16 +0900

    feat: 멤버가 풀었던 오늘의 문제 전체 조회 API 개발 (#50)

--- server/src/main/java/com/pullup/interview/service/InterviewService.java
+++ server/src/main/java/com/pullup/interview/service/InterviewService.java
@@ -8,15 +8,15 @@ import com.pullup.interview.domain.InterviewHint;
 import com.pullup.interview.dto.MyInterviewAnswerDto;
 import com.pullup.interview.dto.request.InterviewAnswerRequest;
 import com.pullup.interview.dto.response.InterviewAnswerResponse;
-import com.pullup.interview.dto.response.MyInterviewAnswersResponse;
 import com.pullup.interview.dto.response.InterviewResponse;
+import com.pullup.interview.dto.response.MyInterviewAnswersResponse;
 import com.pullup.interview.repository.InterviewAnswerRepository;
 import com.pullup.interview.repository.InterviewHintRepository;
 import com.pullup.interview.repository.InterviewRepository;
 import com.pullup.member.domain.Member;
 import com.pullup.member.service.MemberService;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -59,8 +59,15 @@ public class InterviewService {
 
     public MyInterviewAnswersResponse getMyInterviewAnswers(Long memberId) {
         List<InterviewAnswer> interviewAnswers = interviewAnswerRepository.findAllByMemberId(memberId);
-        List<MyInterviewAnswerDto> list = new ArrayList<>();
 
-        return MyInterviewAnswersResponse.of(list);
+        List<MyInterviewAnswerDto> myInterviewAnswerDtos = interviewAnswers.stream()
+                .map(answer -> MyInterviewAnswerDto.of(
+                        answer.getInterview().getId(),
+                        answer.getId(),
+                        answer.getInterview().getQuestion()
+                ))
+                .collect(Collectors.toList());
+
+        return MyInterviewAnswersResponse.of(myInterviewAnswerDtos);
     }
 }
