4fe097d9ce9ff58041101deb05d807a6ce0c7fbf
im2shandyou@gmail.com <104557685+im2sh@users.noreply.github.com>
2025-02-02 22:45:59 +0900

    fix: 메서드 공백 추가 (#52)

--- server/src/main/java/com/pullup/interview/repository/InterviewAnswerRepository.java
+++ server/src/main/java/com/pullup/interview/repository/InterviewAnswerRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface InterviewAnswerRepository extends JpaRepository<InterviewAnswer, Long> {
+
     @Query("SELECT ia FROM InterviewAnswer ia " +
             "JOIN FETCH ia.interview " +
             "WHERE ia.member.id = :memberId")
