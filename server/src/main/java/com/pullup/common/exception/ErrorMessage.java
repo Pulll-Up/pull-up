522a71f91d2c0b772df4aeec078ad00855ffb275
im2shandyou@gmail.com <104557685+im2sh@users.noreply.github.com>
2025-02-03 21:14:29 +0900

    test: branch별 build 테스트 (#60)

--- server/src/main/java/com/pullup/common/exception/ErrorMessage.java
+++ server/src/main/java/com/pullup/common/exception/ErrorMessage.java
@@ -6,7 +6,6 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ErrorMessage {
-
     ERR_EXAM_NOT_FOUND("모의고사를 찾을 수 없습니다"),
     ERR_MEMBER_NOT_FOUND("멤버를 찾을 수 없습니다"),
     ERR_INSUFFICIENT_PROBLEMS("문제 수가 충분하지 않습니다"),
