1981ace2b8d50fe00098cd21eeb87ede944322cc
im2shandyou@gmail.com <104557685+im2sh@users.noreply.github.com>
2025-02-04 14:09:51 +0900

    chore: cors 도메인 추가 (#66)

--- server/src/main/java/com/pullup/common/config/SecurityConfig.java
+++ server/src/main/java/com/pullup/common/config/SecurityConfig.java
@@ -96,7 +96,8 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(Arrays.asList(
-                "http://localhost:5173"
+                "http://localhost:5173",
+                "https://www.pull-up.store"
         ));
 
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"));
