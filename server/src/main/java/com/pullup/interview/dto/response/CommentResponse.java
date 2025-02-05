050aba7d29035fb05b8da3fecf879bd3bdd6ae69
im2shandyou@gmail.com <104557685+im2sh@users.noreply.github.com>
2025-02-05 21:45:45 +0900

    feat: 오늘의 문제에 대한 답변 단건 조회 API 추가 (#68)

--- server/src/main/java/com/pullup/interview/dto/response/CommentResponse.java
+++ server/src/main/java/com/pullup/interview/dto/response/CommentResponse.java
@@ -1,12 +1,17 @@
 package com.pullup.interview.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
 public record CommentResponse(
+        @Schema(description = "댓글 ID", example = "1")
         Long commentId,
+        @Schema(description = "댓글 작성자", example = "이석환")
         String writer,
+        @Schema(description = "댓글 작성자 이메일", example = "email@email.com")
         String email,
+        @Schema(description = "댓글 내용", example = "덕분에 쉽게 이해했습니다 !")
         String content
 ) {
     public static CommentResponse of(Long commentId, String writer, String email, String content) {
--- server/src/main/java/com/pullup/interview/dto/response/InterviewAnswerWithCommentsResponse.java
+++ server/src/main/java/com/pullup/interview/dto/response/InterviewAnswerWithCommentsResponse.java
@@ -1,9 +1,12 @@
 package com.pullup.interview.dto.response;
 
 import com.pullup.interview.dto.InterviewAnswerDto;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 public record InterviewAnswerWithCommentsResponse(
+        @Schema(description = "오늘의 문제 답변")
         InterviewAnswerDto interviewAnswerDto,
+        @Schema(description = "오늘의 문제 답변에 대한 댓글")
         CommentsResponse commentsResponse
 ) {
     public static InterviewAnswerWithCommentsResponse of(InterviewAnswerDto interviewAnswerDto, CommentsResponse commentsResponse) {
