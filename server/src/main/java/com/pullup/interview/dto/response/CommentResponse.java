f6a5472bbc415e06dde4b8169e98169fc732096a
im2shandyou@gmail.com <104557685+im2sh@users.noreply.github.com>
2025-02-05 21:28:36 +0900

    feat: CommentsResponse 추가 (#68)

--- server/src/main/java/com/pullup/interview/dto/response/CommentResponse.java
+++ server/src/main/java/com/pullup/interview/dto/response/CommentResponse.java
@@ -1,4 +1,20 @@
 package com.pullup.interview.dto.response;
 
-public record CommentResponse() {
+import lombok.Builder;
+
+@Builder
+public record CommentResponse(
+        Long commentId,
+        String writer,
+        String email,
+        String content
+) {
+    public static CommentResponse of(Long commentId, String writer, String email, String content) {
+        return CommentResponse.builder()
+                .commentId(commentId)
+                .writer(writer)
+                .email(email)
+                .content(content)
+                .build();
+    }
 }
