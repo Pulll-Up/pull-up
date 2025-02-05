6cbca4fa23c6aab27150d16c0fa4ae6a4555dec3
im2shandyou@gmail.com <104557685+im2sh@users.noreply.github.com>
2025-02-06 02:48:28 +0900

    chore: ModifyCommentRequest 파일명 변경 및 Swagger 스키마 추가 (#71)

--- server/src/main/java/com/pullup/interview/dto/request/ModifyCommentRequest.java
+++ server/src/main/java/com/pullup/interview/dto/request/ModifyCommentRequest.java
@@ -1,9 +1,10 @@
 package com.pullup.interview.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
-import org.springframework.beans.factory.annotation.Value;
 
-public record ModifyCommentResponse(
+public record ModifyCommentRequest(
+        @Schema(description = "많이 배워갑니다.")
         @NotBlank(message = "댓글 내용은 필수입니다.")
         String content
 ) {
