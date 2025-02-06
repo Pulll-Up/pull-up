629f6e048b5acd922f8b9c39d5183cc5b6d94c0e
im2shandyou@gmail.com <104557685+im2sh@users.noreply.github.com>
2025-02-06 19:45:52 +0900

    test: 테스트 추가

--- server/src/main/java/com/pullup/auth/jwt/config/JwtProperties.java
+++ server/src/main/java/com/pullup/auth/jwt/config/JwtProperties.java
@@ -1,5 +1,6 @@
 package com.pullup.auth.jwt.config;
 
+import jakarta.annotation.PostConstruct;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -14,4 +15,13 @@ public class JwtProperties {
     private long accessTokenExpiration;
     private long refreshTokenExpiration;
     private String authRedirectUri;
+
+    @PostConstruct
+    public void logProperties() {
+        System.out.println("JWT Properties Loaded:");
+        System.out.println("Secret: " + secret);
+        System.out.println("Access Token Expiration: " + accessTokenExpiration);
+        System.out.println("Refresh Token Expiration: " + refreshTokenExpiration);
+        System.out.println("Auth Redirect URI: " + authRedirectUri);
+    }
 }
