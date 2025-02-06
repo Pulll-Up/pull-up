bb6c6111eebfc22784caa0dec28f3720dd583c35
im2shandyou@gmail.com <104557685+im2sh@users.noreply.github.com>
2025-02-06 15:09:19 +0900

    test: CI 테스트 (#60)

--- server/src/main/java/com/pullup/common/exception/IllegalStatementException.java
+++ server/src/main/java/com/pullup/common/exception/IllegalStatementException.java
@@ -3,6 +3,7 @@ package com.pullup.common.exception;
 import org.springframework.http.HttpStatus;
 
 public class IllegalStatementException extends PullUpException {
+
     public IllegalStatementException(ErrorMessage errorMessage) {
         super(HttpStatus.INTERNAL_SERVER_ERROR, errorMessage);
     }
