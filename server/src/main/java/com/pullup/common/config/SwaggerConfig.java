f9086bb4da33243ecb391b737cf2ccf46a28d688
im2shandyou@gmail.com <104557685+im2sh@users.noreply.github.com>
2025-02-12 09:32:34 +0900

    chore: Swagger 주소 수정

--- server/src/main/java/com/pullup/common/config/SwaggerConfig.java
+++ server/src/main/java/com/pullup/common/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.pullup.common.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
@@ -11,6 +12,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
+        servers = {
+                @Server(url = "http://localhost:8080", description = "Local Server"),
+                @Server(url = "https://api.pull-up.store", description = "Production Server")
+        },
         info = @Info(
                 title = "PullUp API",
                 description = "PullUp API 명세서",
