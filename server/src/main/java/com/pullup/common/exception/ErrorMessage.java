e965d25031b86e531e9a9f88f2a605397f59e66d
king0104 <king0104@konkuk.ac.kr>
2025-02-12 10:34:14 +0900

    fix: errormessage 수정

--- server/src/main/java/com/pullup/common/exception/ErrorMessage.java
+++ server/src/main/java/com/pullup/common/exception/ErrorMessage.java
@@ -41,7 +41,8 @@ public enum ErrorMessage {
 
     ERR_PLAYER_NOT_FOUND("플레이어를 찾을 수 없습니다"),
 
-    //* 서버 에러
+    // TODO : 명확한 네이밍으로 수정 필요
+    //* Server 내부 Exception
     ERR_INTERNAL_SERVER("알 수 없는 서버의 오류입니다");
 
     private final String message;
