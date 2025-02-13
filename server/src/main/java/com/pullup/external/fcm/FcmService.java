cc52d04da437c6bc7e65bd6328455feabc6340c1
im2shandyou@gmail.com <104557685+im2sh@users.noreply.github.com>
2025-02-13 17:41:41 +0900

    feat: 알림 전송 재시도 로직 추가 (#134)

--- server/src/main/java/com/pullup/external/fcm/FcmService.java
+++ server/src/main/java/com/pullup/external/fcm/FcmService.java
@@ -1,10 +1,15 @@
 package com.pullup.external.fcm;
 
+import static com.google.firebase.messaging.MessagingErrorCode.SENDER_ID_MISMATCH;
+import static com.google.firebase.messaging.MessagingErrorCode.THIRD_PARTY_AUTH_ERROR;
+import static com.google.firebase.messaging.MessagingErrorCode.UNREGISTERED;
+import static com.google.rpc.Code.INVALID_ARGUMENT;
 import static com.pullup.common.exception.ErrorMessage.ERR_FCM_FAILED_TO_SEND;
 
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.MessagingErrorCode;
 import com.google.firebase.messaging.Notification;
 import com.pullup.common.exception.InternalServerException;
 import com.pullup.member.domain.DeviceToken;
@@ -12,9 +17,12 @@ import com.pullup.member.repository.DeviceTokenRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -23,6 +31,10 @@ public class FcmService {
     private final FirebaseMessaging firebaseMessaging;
     private final DeviceTokenRepository deviceTokenRepository;
 
+    @Retryable(
+            retryFor = {FirebaseMessagingException.class},
+            backoff = @Backoff(delay = 1000, multiplier = 2)
+    )
     public void sendNotifications() {
         List<DeviceToken> deviceTokens = deviceTokenRepository.findAll();
 
@@ -31,6 +43,8 @@ public class FcmService {
                 sendFcmMessage(deviceToken.getToken(), "하루 1문제, 당신의 실력이 됩니다!",
                         "매일 한 문제씩! 작은 습관이 큰 차이를 만듭니다. 오늘의 문제를 확인하세요.");
             } catch (FirebaseMessagingException e) {
+                MessagingErrorCode errorCode = e.getMessagingErrorCode();
+                log.info("errorCode: {}", errorCode);
                 throw new InternalServerException(ERR_FCM_FAILED_TO_SEND);
             }
         });
