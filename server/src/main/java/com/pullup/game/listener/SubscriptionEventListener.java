4384ca20d7e5d967d2beac3cf42d54e0cfb191e0
king0104 <king0104@konkuk.ac.kr>
2025-02-14 12:50:07 +0900

    feat: 구독 취소 시 로그 찍히도록 수정

--- server/src/main/java/com/pullup/game/listener/SubscriptionEventListener.java
+++ server/src/main/java/com/pullup/game/listener/SubscriptionEventListener.java
@@ -20,11 +20,13 @@ import java.util.concurrent.atomic.AtomicInteger;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 import org.springframework.web.socket.messaging.SessionSubscribeEvent;
+import org.springframework.web.socket.messaging.SessionUnsubscribeEvent;
 
 @Slf4j
 @Component
@@ -114,6 +116,15 @@ public class SubscriptionEventListener {
                         gameRoomInfoWithProblemsResponse);
             }
         }
+    }
+
+    @EventListener
+    public void handleUnsubscribeEvent(SessionUnsubscribeEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String sessionId = headerAccessor.getSessionId();
+        String destination = (String) headerAccessor.getHeader(SimpMessageHeaderAccessor.DESTINATION_HEADER);
+
+        log.info("구독 취소 감지 - 세션 ID: {}, 구독 취소된 destination: {}", sessionId, destination);
     }
 
 
