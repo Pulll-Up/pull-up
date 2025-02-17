845d145e66edccbebf02dfd0bad09048e72a0af2
송하은 <she3469@gmail.com>
2025-02-17 13:20:04 +0900

    feat: 레이아웃 fallback 및 페이지 lazy loading 적용

---
+++ client/src/components/fallbacks/spinners/PageSpinner.tsx
@@ -0,0 +1,9 @@
+const PageSpinner = () => {
+  return (
+    <div className="flex h-full items-center justify-center bg-Main">
+      <span className="text-lg font-semibold">PageSpinner</span>
+    </div>
+  );
+};
+
+export default PageSpinner;
