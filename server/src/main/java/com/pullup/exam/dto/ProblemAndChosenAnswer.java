c0fbf820d106eaacd38b9adffc26a15bda18ff90
king0104 <king0104@konkuk.ac.kr>
2025-02-18 21:18:32 +0900

    fix: 모의고사 문제 id 복호화 어노테이션 작성

--- server/src/main/java/com/pullup/exam/dto/ProblemAndChosenAnswer.java
+++ server/src/main/java/com/pullup/exam/dto/ProblemAndChosenAnswer.java
@@ -1,6 +1,9 @@
 package com.pullup.exam.dto;
 
+import com.pullup.common.annotation.DecryptedId;
+
 public record ProblemAndChosenAnswer(
+        @DecryptedId
         Long problemId,
         String chosenAnswer
 ) {
