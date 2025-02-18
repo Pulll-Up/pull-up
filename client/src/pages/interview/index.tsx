98182889384407679de422e1f42524d292394255
antis <rkdwldms42@naver.com>
2025-02-18 15:14:50 +0900

    fix: 답안 제출 시 LoadingPage 화면 띄움

--- client/src/pages/interview/index.tsx
+++ client/src/pages/interview/index.tsx
@@ -9,6 +9,7 @@ import { memberStore } from '@/stores/memberStore';
 import { getMember } from '@/api/member';
 import { queryClient } from '@/main';
 import { Member } from '@/types/member';
+import LoadingPage from '@/pages/loading';
 
 const InterviewPage = () => {
   const navigate = useNavigate();
@@ -18,6 +19,7 @@ const InterviewPage = () => {
 
   const [hint, setHint] = useState(false);
   const [answer, setAnswer] = useState('');
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
   useEffect(() => {
     if (isSolvedToday) {
@@ -53,6 +55,8 @@ const InterviewPage = () => {
       return;
     }
 
+    setIsSubmitting(true);
+
     // 답안 제출
     const response = await createAnswer({
       interviewId: data.interviewId,
@@ -62,6 +66,8 @@ const InterviewPage = () => {
     setInterviewAnswerId(response.interviewAnswerId);
     setIsSolvedToday(true);
     navigate(`/interview/result/${response.interviewAnswerId}`);
+
+    setIsSubmitting(false);
   };
 
   const onKeyDown = (e: TextAreaKeyboardEvent) => {
@@ -81,27 +87,33 @@ const InterviewPage = () => {
   };
 
   return (
-    <div className="flex min-h-full w-full items-center justify-center bg-gradient-to-b from-primary-50 to-white p-6 md:p-10">
-      <div className="flex w-[873px] flex-col items-center justify-center gap-12 pt-[94px] sm:pt-16">
-        <div className="text-xl font-extrabold md:text-2xl lg:text-3xl">
-          <span className="text-primary-600">{`${member.name}`}</span>
-          <span>님 만을 위한 오늘의 맞춤 문제🎯</span>
+    <>
+      {isSubmitting ? (
+        <LoadingPage />
+      ) : (
+        <div className="flex min-h-full w-full items-center justify-center bg-gradient-to-b from-primary-50 to-white p-6 md:p-10">
+          <div className="flex w-[873px] flex-col items-center justify-center gap-12 pt-[94px] sm:pt-16">
+            <div className="text-xl font-extrabold md:text-2xl lg:text-3xl">
+              <span className="text-primary-600">{`${member.name}`}</span>
+              <span>님 만을 위한 오늘의 맞춤 문제🎯</span>
+            </div>
+            <InterviewCard title={data.question} keywords={data.keywords} hint={hint} onHintClick={onHintClick} />
+            <div className="flex w-full flex-col justify-start gap-2">
+              <span className="text-lg font-semibold lg:text-xl">나의 답변</span>
+              <InputForm
+                id="todayQuestion"
+                placeholder="질문에 대한 답을 남겨주세요."
+                value={answer}
+                limit={500}
+                onChange={onChange}
+                onSubmit={onSubmit}
+                onKeyDown={onKeyDown}
+              />
+            </div>
+          </div>
         </div>
-        <InterviewCard title={data.question} keywords={data.keywords} hint={hint} onHintClick={onHintClick} />
-        <div className="flex w-full flex-col justify-start gap-2">
-          <span className="text-lg font-semibold lg:text-xl">나의 답변</span>
-          <InputForm
-            id="todayQuestion"
-            placeholder="질문에 대한 답을 남겨주세요."
-            value={answer}
-            limit={500}
-            onChange={onChange}
-            onSubmit={onSubmit}
-            onKeyDown={onKeyDown}
-          />
-        </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
