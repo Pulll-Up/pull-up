a9a172d5d616ff2d5741954e086d29c4c41484be
songhaeunsong <she3469@gmail.com>
2025-02-20 19:32:04 +0900

    publish: spinner 디자인

--- client/src/components/fallbacks/spinners/PageSpinner.tsx
+++ client/src/components/fallbacks/spinners/PageSpinner.tsx
@@ -1,7 +1,11 @@
 const PageSpinner = () => {
   return (
     <div className="flex h-full items-center justify-center bg-Main">
-      <span className="text-lg font-semibold">PageSpinner</span>
+      <div className="relative h-24 py-5">
+        <div className="spin-pause absolute flex h-12 w-12 items-center justify-center rounded bg-primary-500 text-4xl font-extrabold text-white">
+          P
+        </div>
+      </div>
     </div>
   );
 };
